b70a4956f675653bba22f03fe18e3b8f0d23803b
Leandher <leandher_bessa@hotmail.com>
2020-08-09 22:48:39 -0300

    chore: configure jest

---
+++ packages/server/jest.config.js
@@ -0,0 +1,18 @@
+const { pathsToModuleNameMapper } = require('ts-jest/utils');
+
+const { compilerOptions } = require('./tsconfig.json');
+
+module.exports = {
+  clearMocks: true,
+  testRegex: '.spec.ts$',
+  transform: {
+    '^.+\\.(t|j)s$': 'ts-jest',
+  },
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
+  testEnvironment: 'node',
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, { prefix: "<rootDir>" }),
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  moduleFileExtensions: ['js', 'ts'],
+};
