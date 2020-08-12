1e0c156d47d9d524bcde143885df811c1d065a21
Leandher <leandher_bessa@hotmail.com>
2020-08-11 22:50:23 -0300

    feature: added ensureAuthenticated

--- packages/server/src/modules/classes/infra/http/routes/index.ts
+++ packages/server/src/modules/classes/infra/http/routes/index.ts
@@ -2,8 +2,11 @@ import express from 'express';
 
 import { createClassController } from '@modules/classes/useCases/createClass';
 import { findClassesController } from '@modules/classes/useCases/findClasses';
+import ensureAuthenticated from '@modules/users/infra/http/middlewares/ensureAuthenticated';
 
 const classRouter = express.Router();
+
+classRouter.use(ensureAuthenticated);
 
 classRouter.get('/', (request, response) => findClassesController.handle(request, response));
 
